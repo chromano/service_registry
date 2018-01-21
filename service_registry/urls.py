8f24702210ca1ca15a44fe25ea0ada8860b80083
Carlos Henrique Romano <chromano@gmail.com>
2018-01-21 14:51:05 -0200

    Install API app on service_registry project

--- service_registry/urls.py
+++ service_registry/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from api.views import ServiceViewSet
+
+router = routers.SimpleRouter()
+router.register(r'services/(?P<name>[\w]+)(?:/(?P<version>[\w\.]+))?',
+                ServiceViewSet, base_name='services')
 
 urlpatterns = [
+    path('api/v1/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
